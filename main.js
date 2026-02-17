6bb21143083e01a4ac36d1e7c3e0ec9adac67bea
MGC-ceo <malachicolvinmgc@gmail.com>
2026-02-17 01:18:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ function loadProgress(){
 
 let playerLevel = loadProgress();
 
-/* ================= SIMPLE SOUND ENGINE ================= */
+/* ================= SOUND ENGINE ================= */
 
-function playBeep(scene, freq = 440, duration = 100){
+function tone(scene, freq = 440, duration = 100, volume = 0.2){
   const ctx = scene.sound.context;
   if(!ctx) return;
 
@@ -41,11 +41,42 @@ function playBeep(scene, freq = 440, duration = 100){
   osc.frequency.value = freq;
   osc.type = "sine";
 
-  gain.gain.setValueAtTime(0.2, ctx.currentTime);
+  gain.gain.setValueAtTime(volume, ctx.currentTime);
   gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + duration/1000);
 
   osc.start();
   osc.stop(ctx.currentTime + duration/1000);
+}
+
+/* ========== BACKGROUND MUSIC LOOPS ========== */
+
+let menuMusicLoop;
+let gameMusicLoop;
+
+function startMenuMusic(scene){
+  stopGameMusic();
+  menuMusicLoop = scene.time.addEvent({
+    delay: 900,
+    loop: true,
+    callback: () => tone(scene, 220, 400, 0.05)
+  });
+}
+
+function stopMenuMusic(){
+  if(menuMusicLoop) menuMusicLoop.remove();
+}
+
+function startGameMusic(scene){
+  stopMenuMusic();
+  gameMusicLoop = scene.time.addEvent({
+    delay: 700,
+    loop: true,
+    callback: () => tone(scene, 140, 300, 0.05)
+  });
+}
+
+function stopGameMusic(){
+  if(gameMusicLoop) gameMusicLoop.remove();
 }
 
 /* ================= MENU SCENE ================= */
@@ -60,19 +91,7 @@ MenuScene.prototype.create = function(){
 
   this.cameras.main.fadeIn(600);
 
- // Background particles (Phaser 3.90 compatible)
-
-const particles = this.add.particles(0, 0, null, {
-  x: { min: 0, max: this.cameras.main.width },
-  y: this.cameras.main.height,
-  lifespan: 6000,
-  speedY: { min: -20, max: -50 },
-  scale: { start: 0.4, end: 0 },
-  quantity: 2,
-  blendMode: 'ADD'
-});
-
-  /* ===== UI PANEL ===== */
+  startMenuMusic(this);
 
   const panel = this.add.rectangle(centerX,centerY,500,450,0x000000,0.4)
     .setStrokeStyle(2,0x00ffcc);
@@ -86,8 +105,7 @@ const particles = this.add.particles(0, 0, null, {
     .setOrigin(0.5)
     .setInteractive();
 
-  const more = this.add.text(0,50,"More Content Soon",{fontSize:"16px",color:"#888"})
-    .setOrigin(0.5);
+  const more = this.add.text(0,50,"More Content Soon",{fontSize:"16px",color:"#888"}).setOrigin(0.5);
 
   const partyBtn = this.add.text(0,120,"PARTY",{fontSize:"28px",color:"#ffffff"})
     .setOrigin(0.5)
@@ -105,15 +123,15 @@ const particles = this.add.particles(0, 0, null, {
   });
 
   playBtn.on("pointerdown", ()=>{
-    playBeep(this,700,120);
+    tone(this, 700, 120);
+    stopMenuMusic();
     this.cameras.main.fadeOut(400);
     this.time.delayedCall(400,()=>this.scene.start("SoloScene"));
   });
 
   partyBtn.on("pointerdown", ()=>{
-    playBeep(this,500,120);
-    this.cameras.main.fadeOut(400);
-    this.time.delayedCall(400,()=>this.scene.start("PartyScene"));
+    tone(this, 500, 120);
+    this.scene.start("PartyScene");
   });
 };
 
@@ -143,7 +161,7 @@ PartyScene.prototype.create = function(){
     .setInteractive();
 
   backBtn.on("pointerdown", ()=>{
-    playBeep(this,400,120);
+    tone(this, 400, 120);
     this.scene.start("MenuScene");
   });
 };
@@ -171,6 +189,7 @@ let laserGraphics,moneyText,waveText,selectText,hpText,towerCountText;
 SoloScene.prototype.create = function(){
 
   this.cameras.main.fadeIn(400);
+  startGameMusic(this);
 
   enemies=[];
   towers=[];
@@ -180,7 +199,6 @@ SoloScene.prototype.create = function(){
   selectedTower="circle";
 
   drawPath(this);
-
   laserGraphics=this.add.graphics();
 
   moneyText=this.add.text(10,10,"Money: "+money,{color:"#fff"});
@@ -190,9 +208,21 @@ SoloScene.prototype.create = function(){
   towerCountText=this.add.text(10,90,"Towers: 0 / "+MAX_TOWERS,{color:"#aaa"});
 
   this.input.on("pointerdown",p=>tryPlaceTower(this,p.x,p.y));
-  this.input.keyboard.on("keydown-ONE",()=>changeSelection("circle"));
-  this.input.keyboard.on("keydown-TWO",()=>changeSelection("square"));
-  this.input.keyboard.on("keydown-THREE",()=>changeSelection("triangle"));
+
+  this.input.keyboard.on("keydown-ONE",()=>{
+    tone(this,600,80);
+    changeSelection("circle");
+  });
+
+  this.input.keyboard.on("keydown-TWO",()=>{
+    tone(this,500,80);
+    changeSelection("square");
+  });
+
+  this.input.keyboard.on("keydown-THREE",()=>{
+    tone(this,400,80);
+    changeSelection("triangle");
+  });
 
   spawnWave(this);
   this.time.addEvent({delay:7000,loop:true,callback:()=>spawnWave(this)});
@@ -206,6 +236,8 @@ SoloScene.prototype.update = function(time){
 /* ================= GAME LOGIC ================= */
 
 function spawnWave(scene){
+  tone(scene,300,200); // wave start sound
+
   for(let i=0;i<5+wave;i++){
     const e={x:path[0].x,y:path[0].y,hp:12+wave*3,speed:0.8,pathIndex:0,alive:true};
     e.body=scene.add.circle(e.x,e.y,14,0xff5555);
@@ -274,14 +306,13 @@ function updateTowerDamage(scene,time){
       const dx=e.x-t.x,dy=e.y-t.y;
       if(dx*dx+dy*dy<=t.range*t.range){
 
-        // LASER VISUAL
         laserGraphics.lineStyle(2,0xffffff,0.7);
         laserGraphics.beginPath();
         laserGraphics.moveTo(t.x,t.y);
         laserGraphics.lineTo(e.x,e.y);
         laserGraphics.strokePath();
 
-        playBeep(scene,800,50);
+        tone(scene,800,50); // tower attack sound
         damageEnemy(e,t.dmg);
       }
     });
@@ -304,6 +335,8 @@ function damageCrystal(scene,amount){
   crystalHP-=amount;
   hpText.setText("Crystal HP: "+crystalHP);
   if(crystalHP<=0){
+    stopGameMusic();
+    tone(scene,120,400,0.4); // lose sound
     scene.scene.start("MenuScene");
   }
 }
